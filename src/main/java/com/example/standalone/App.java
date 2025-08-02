12b5c7691709094796ec5afd92b994f9c1973707
pranav2031 <pranavsonawane562@gmail.com>
2025-08-02 16:07:44 +0530

    first commit

---
+++ src/main/java/com/example/standalone/App.java
@@ -0,0 +1,13 @@
+package com.example.standalone;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Docker springboot application" );
+    }
+}
